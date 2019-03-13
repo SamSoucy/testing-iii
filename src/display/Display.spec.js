c8bb7917efd05c943cc34e2d4671cc283b391157
Samsoucy <newf930@outlook.com>
2019-03-13 18:34:48 -0400

    added test in display for gate open/closed and locked /unlocked

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,7 +1,8 @@
 // Test away!
 
 import React from 'react';
-import Display from './Display';
+import Display from "./Display"
+import Controls from '../controls/Controls';
 import { render} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
@@ -9,4 +10,20 @@ describe("The Display", () => {
     it("renders without crashing", () => {
         render(<Display />);
     });
+
+    it("displays if gate is open/closed and if it is locked/unlocked", () => {
+        const { getByText } = render(<Display open={true} closed={true} locked={true} unlocked={true} />);
+  
+        getByText(/closed/i);
+        getByText(/open/i);
+        getByText(/locked/i);
+        getByText(/unlocked/i);
+    });
+    
+    // it("Gate is locked or unlocked", () => {
+    //     const { getByText } = render(<Display locked={true} unlocked={true} />);
+
+    //     getByText(/locked/i);
+    //     getByText(/unlocked/i);
+    // });
 })
